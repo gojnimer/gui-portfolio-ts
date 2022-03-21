43790aa303344dd21db84785fdb0a9df865d4680
gojnimer <gojnimer@gmail.com>
2022-03-21 01:17:39 -0300

    FEAT: TESTE ESTEIRA

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TESTE ESTEIRA
         </a>
       </header>
     </div>
